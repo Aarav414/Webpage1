aa74f4e695b3cf0b84e66b39eb9cca8a497485eb
Aarav414 <84316227+Aarav414@users.noreply.github.com>
2021-05-17 19:17:51 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor() {
+    document.getElementById("heading1").style.color = "red";
+}
+
+function setdate() {
+    document.getElementById("date").innerHTML = new Date().toDateString();
+}
